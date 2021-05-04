450713976dc929e2ce34a75574cd71bb8a4c73c3
Clement Liscoet <clement.liscoet@gmail.com>
2021-05-04 19:47:23 +0100

    feat:server ready to be use

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config({ path: "./config/config.env" });
+require("colors");
 
-const env = require("dotenv").config({ path: "./config/config.env" });
-// console.log(env.parsed);
 const PORT = process.env.PORT;
-console.log(PORT);
+
+const app = express();
+
+app.use(cors());
+
+app.listen(PORT, (err) => {
+  try {
+    console.log(`Server running on PORT: ${PORT}`.yellow.inverse);
+  } catch (err) {
+    console.log(`An error happned ... ${err.message}`.red.inverse);
+  }
+});
